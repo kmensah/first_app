a7a94eb394246984439edeb91d66d33b7866d03b
Kojo Mensah <kojo.mensah.jr@gmail.com>
2011-06-19 16:27:49 -0400

    First Love

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e035cbe407ca5a07d44a8b360e706bb3574fdd88f850c076350ed4f1bb2a5ee2db337110e320cf26418e6dd581d364caef864414f7e2094c4c2a7660d840120e'
